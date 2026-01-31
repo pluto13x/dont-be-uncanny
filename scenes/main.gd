32138980fc462ef39dcfa9ddf9ba2908c6b952ae
pluto13x <saramilosavljevic6@gmail.com>
2026-01-31 18:47:57 +0100

    added and connected all 4 characters

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,18 +1,13 @@
 extends Node2D
 
-var chosen_character = ""
-
 func _start_interacting(input_signal):
 	if input_signal == "chosen_character":
 		$top_layer/socialization_ui.visible = true
 		
-func _character_chosen(character):
-	chosen_character = character
 
 func _ready() -> void:
-	Dialogic.signal_event.connect(_character_chosen)
 	Dialogic.signal_event.connect(_start_interacting)
 
 func _process(delta: float) -> void:
-	
+	print(Global.chosen_character)
 	pass
--- scenes/uncanny_bar.gd
+++ scenes/uncanny_bar.gd
@@ -1,14 +1,12 @@
 extends ProgressBar
 
-var chosen_character = "barista"
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	match chosen_character:
+	match Global.chosen_character:
 		"barista":
 			value = Dialogic.VAR.barista_uncanny
 		"cook":
